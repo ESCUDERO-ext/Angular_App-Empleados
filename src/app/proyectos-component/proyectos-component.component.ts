93dca7c024ff45b770f6e137522727b4c3bc1883
ESCUDERO-ext <christian.escudero.external@eviden.com>
2025-04-24 16:11:06 +0200

    Ejercicio Routing III

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { EmpleadosService } from '../empleados.service';
+import { Empleado } from '../empeado.model';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -6,6 +10,35 @@ import { Component } from '@angular/core';
   templateUrl: './proyectos-component.component.html',
   styleUrl: './proyectos-component.component.css'
 })
-export class ProyectosComponentComponent {
+export class ProyectosComponentComponent implements OnInit {
+
+  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService){}
+
+  empleados:Empleado[]=[];
+
+  ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
+  }
+
+  volverHome() {
+  
+    this.router.navigate(['']); //Para volver al home es vacío '' porque así es como lo hemos declarado
+
+  }
+
+  agregarEmpleado(){
+  
+      let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+      //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre);
+      this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+
+      this.router.navigate(['']);
+
+    }
+  
+    cuadroNombre:string="";
+    cuadroApellido:string="";
+    cuadroCargo:string="";
+    cuadroSalario:number=0;
 
 }
